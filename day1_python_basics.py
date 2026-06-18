ef47943b3f1c5e1db3f338e632c08c812559e6f8
Athiya <athiyataj9@gmail.com>
2026-06-18 11:30:35 +0530

    Add day1 of python practice

---
+++ day1_python_basics.py
@@ -0,0 +1,34 @@
+# ==========================================
+# DAY 1: VARIABLES & DATA TYPES 🧪
+# ==========================================
+
+# 1. STRINGS (Text)
+# Labeled jar for words.
+name = "Athiya" 
+
+# 2. INTEGERS (Whole Numbers)
+# Counting whole units.
+age = 22 
+
+# 3. FLOATS (Decimals)
+# Precise measurements for math/data.
+gpa = 3.8 
+
+# 4. BOOLEANS (Logical)
+# True or False switches.
+is_coding = True 
+
+print("--- Day 1: Results ---")
+print(f"User: {name} | Age: {age} | GPA: {gpa}")
+
+# 5. TYPE CHECKING
+# Asking Python what's inside the jar.
+print(f"Data type of name: {type(name)}")
+
+# 6. TYPE CASTING
+# Converting a string number into an actual integer for math.
+user_input = "10"
+converted_input = int(user_input)
+print(f"Math Result: {converted_input + 5}")
+
+# End of Day 1 Script
